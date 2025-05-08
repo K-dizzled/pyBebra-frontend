b6820e0a7256cb5f9df680a9b941cb024823b713
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2025-05-08 03:37:33 +0200

    Ggwp

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,9 @@
-import { Search, Settings, HelpCircle } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="bg-[#2b2d30] border-b border-[#323438] py-3 px-4">
-      <div className="container mx-auto flex items-center justify-between">
+    <header className="bg-[#2b2d30] border-b border-[#323438] py-3">
+      <div className="container mx-auto flex items-center justify-between px-4">
         <div className="flex items-center">
           <div className="flex items-center mr-8">
             <Image
